6b70e77f97536626dffceac6011e90fc88f714f4
Barak Michener <me@barakmich.com>
2020-09-21 18:18:32 -0700

    update models and include fsmodel

--- models/generate.sh
+++ models/generate.sh
@@ -2,4 +2,5 @@
 
 GOGOPROTO_ROOT="${GOPATH}/src"
 GOGOPROTO_PATH="${GOGOPROTO_ROOT}:${GOGOPROTO_ROOT}/protobuf"
-protoc --gogofaster_out=plugins=grpc:. -I=.:"${GOGOPROTO_PATH}" *.proto
+#protoc --gogofaster_out=plugins=grpc:. -I=.:"${GOGOPROTO_PATH}" *.proto
+protoc -I=. -I=$GOPATH/src -I=$GOPATH/src/github.com/gogo/protobuf/protobuf --gogofaster_out=plugins=grpc:. *.proto
